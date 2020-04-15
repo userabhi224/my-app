372789321aa010cc48c5a2b87a8853a1a40aa3ca
userabhi224 <62999704+userabhi224@users.noreply.github.com>
2020-04-15 12:25:15 +0530

    Update button.stories.tsx

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -3,14 +3,10 @@ import { storiesOf } from "@storybook/react";
 import Button from "./Button";
 
 storiesOf("Button", module)
-    .add("Simple button", () => <Button backgroundColor="white" disabled={false}/>
-    )
+    .add("Simple button", () => <Button backgroundColor="White" disabled={false}/>)
 
-    .add("Different Color", () => <Button backgroundColor="Cyan" disabled={false}/>
-    )
+    .add("Different Color", () => <Button backgroundColor="Red" disabled={false}/>)
 
-    .add("Disabled button", () => <Button backgroundColor="White" disabled={true}/>
-    )
+    .add("Disabled button", () => <Button backgroundColor="White" disabled={true}/>)
 
-    .add("Button with icon", () => <div><Button backgroundColor="White" disabled={false}/>👍</div>
-    )
+    .add("Button with icon", () => <div><Button backgroundColor="White" disabled={false}/>👍</div>)
