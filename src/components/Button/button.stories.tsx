978a89231a0056b59895e5232ddb797e9a6698d0
userabhi224 <62999704+userabhi224@users.noreply.github.com>
2020-04-22 10:55:12 +0530

    added knobs addon

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Button from "./Button";
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { boolean } from "@storybook/addon-knobs"
+import { actions } from "@storybook/addon-actions"
+import Button from "./Button"
 
 storiesOf("Button", module)
-    .add("Simple button", () => <Button backgroundColor="White" disabled={false}/>)
+    .add("Simple button", () => <Button backgroundColor="White" disabled={boolean("Disabled", false)}/>)
 
-    .add("Different Color", () => <Button backgroundColor="Green" disabled={false}/>)
+    .add("Different Color", () => <Button backgroundColor="Green" disabled={boolean("Disabled", false)}/>)
 
-    .add("Disabled button", () => <Button backgroundColor="White" disabled={true}/>)
+    .add("Disabled button", () => <Button backgroundColor="White" disabled={boolean("Disabled", false)}/>)
 
     .add("Button with icon", () => <div><Button backgroundColor="White" disabled={false}/>👍</div>)
